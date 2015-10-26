c82dfed817f6c4598b6aad48724e77eb996cf258
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2015-10-26 17:23:41 +0000

    settings.py: added default permission settings for API
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- crayonbox/settings.py
+++ crayonbox/settings.py
@@ -54,6 +54,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
