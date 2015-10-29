773b40eb780761598fb5df84d0a0ab10f60d00d9
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2015-10-29 16:15:11 +0000

    Added user profile as main view
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- crayonbox/urls.py
+++ crayonbox/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from userprofile import views
+
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^api/', include('api.urls')),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
+    url(r'^logout', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
