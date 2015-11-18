ad413a020b4e42fa82bf0d4fd23c4325d718a370
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2015-11-18 21:08:33 +0000

    API: added support for ordering manifests by ID
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- crayonbox/settings.py
+++ crayonbox/settings.py
@@ -55,7 +55,9 @@ INSTALLED_APPS = (
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
