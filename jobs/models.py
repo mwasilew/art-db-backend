c64bd30c976b17636852a1d55e9d3b89ef5fd735
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-05 12:21:23 +0100

    url and name for BuildJob are required

--- jobs/models.py
+++ jobs/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class BuildJob(models.Model):
     id = models.CharField(primary_key=True, max_length=100)
 
-    url = models.URLField(blank=True)
-    name = models.CharField(blank=True, max_length=256)
+    name = models.CharField(max_length=256)
+    url = models.URLField()
 
     manifest = models.TextField(blank=True)
     branch_name = models.CharField(blank=True, max_length=256)
