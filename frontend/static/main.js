1847474b390b6f4a8483c681d5131bb662058243
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-05 10:26:22 +0100

    presentation of build jobs submitted

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -3,7 +3,15 @@ var app = angular.module('art', ['ngMaterial']);
 app.controller('Toolbar', ['$scope', '$http', function($scope, $http) {
 
     $http.get('/api/token/').then(function(response) {
-        $scope.token = response.data.key;
+        $scope.auth = response.data;
+    });
+
+}]);
+
+app.controller('JobsList', ['$scope', '$http', function($scope, $http) {
+
+    $http.get('/api/build/').then(function(response) {
+        $scope.builds = response.data;
     });
 
 }]);
