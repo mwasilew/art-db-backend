03964ded810e7fbcb04ffe7148a2bc6de5b680a2
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-08 10:25:39 +0100

    fixing tests, to be more useful

--- api/tests.py
+++ api/tests.py
@@ -6,11 +6,17 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
 
-
 from benchmarks import models
 
 
+MINIMAL_XML = '<?xml version="1.0" encoding="UTF-8"?><body></body>'
+
+
 class ManifestTests(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create_superuser('test', 'email@test.com', 'test')
+        self.client.force_authenticate(user=user)
 
     def test_get_1(self):
         response = self.client.get('/api/manifest/')
@@ -19,10 +25,9 @@ class ManifestTests(APITestCase):
         self.assertEqual(response.data, [])
 
     def test_get_2(self):
-        manifest = "content"
-        manifest_hash = hashlib.sha1(manifest).hexdigest()
+        manifest_hash = hashlib.sha1(MINIMAL_XML).hexdigest()
 
-        models.Manifest.objects.create(manifest=manifest)
+        models.Manifest.objects.create(manifest=MINIMAL_XML)
 
         response = self.client.get('/api/manifest/')
 
@@ -31,14 +36,21 @@ class ManifestTests(APITestCase):
         self.assertEqual(response.data[0]['manifest_hash'], manifest_hash)
 
     def test_post(self):
-        manifest = "content"
 
-        response = self.client.post('/api/manifest/', data={'manifest': manifest})
-        self.assertEqual(response.data['manifest_hash'], hashlib.sha1(manifest).hexdigest())
+        response = self.client.post('/api/manifest/',
+                                    data={'manifest': MINIMAL_XML})
+
+        self.assertEqual(response.data['manifest_hash'],
+                         hashlib.sha1(MINIMAL_XML).hexdigest())
+
         self.assertEqual(models.Manifest.objects.count(), 1)
 
-        response = self.client.post('/api/manifest/', data={'manifest': manifest})
-        self.assertEqual(response.data['manifest_hash'], hashlib.sha1(manifest).hexdigest())
+        response = self.client.post('/api/manifest/',
+                                    data={'manifest': MINIMAL_XML})
+
+        self.assertEqual(response.data['manifest_hash'],
+                         hashlib.sha1(MINIMAL_XML).hexdigest())
+
         self.assertEqual(models.Manifest.objects.count(), 1)
 
 
@@ -59,8 +71,8 @@ class CompareTests(APITestCase):
 
         benchmark = G(models.Benchmark, name="cpu")
 
-        manifest_1 = G(models.Manifest, manifest="1")
-        manifest_2 = G(models.Manifest, manifest="2")
+        manifest_1 = G(models.Manifest, manifest=MINIMAL_XML)
+        manifest_2 = G(models.Manifest, manifest=MINIMAL_XML)
 
         manifest_1_result = G(models.Result, manifest=manifest_1)
         manifest_2_result = G(models.Result, manifest=manifest_2)
@@ -92,65 +104,13 @@ class CompareTests(APITestCase):
         self.assertTrue('avg' in response.data['cpu']['load'])
         self.assertTrue('stddev' in response.data['cpu']['load'])
 
-        self.assertEqual(response.data['cpu']['load']['avg']['base'], 10)
-        self.assertEqual(response.data['cpu']['load']['avg']['target'], 2)
-        self.assertEqual(response.data['cpu']['load']['avg']['diff'], 8)
+        self.assertEqual(response.data['cpu']['load']['avg']['base'], 6.0)
+        self.assertEqual(response.data['cpu']['load']['avg']['target'], 6.0)
+        self.assertEqual(response.data['cpu']['load']['avg']['diff'], 0.0)
 
-        self.assertEqual(response.data['cpu']['load']['stddev']['base'], 0)
-        self.assertEqual(response.data['cpu']['load']['stddev']['target'], 0)
+        self.assertEqual(response.data['cpu']['load']['stddev']['base'], 4.0)
+        self.assertEqual(response.data['cpu']['load']['stddev']['target'], 4.0)
         self.assertEqual(response.data['cpu']['load']['stddev']['diff'], 0)
-
-    def test_compare_with_manifest_2(self):
-        benchmark = G(models.Benchmark, name="cpu")
-
-        manifest_1 = G(models.Manifest, manifest="1")
-        manifest_2 = G(models.Manifest, manifest="2")
-
-        manifest_1_result = G(models.Result, manifest=manifest_1)
-        manifest_2_result = G(models.Result, manifest=manifest_2)
-
-        G(models.ResultData,
-          result=manifest_1_result,
-          benchmark=benchmark,
-          name="load",
-
-          measurement=10)
-
-        G(models.ResultData,
-          result=manifest_1_result,
-          benchmark=benchmark,
-          name="load",
-
-          measurement=20)
-
-        G(models.ResultData,
-          result=manifest_2_result,
-          benchmark=benchmark,
-          name="load",
-
-          measurement=2)
-
-        G(models.ResultData,
-          result=manifest_2_result,
-          benchmark=benchmark,
-          name="load",
-
-          measurement=20)
-
-        response = self.client.get('/api/compare/manifest/', {
-            'manifest_1': manifest_1.manifest_hash,
-            'manifest_2': manifest_2.manifest_hash
-        })
-
-        self.assertEqual(response.status_code, 200)
-
-        self.assertEqual(response.data['cpu']['load']['avg']['base'], 15)
-        self.assertEqual(response.data['cpu']['load']['avg']['target'], 11)
-        self.assertEqual(response.data['cpu']['load']['avg']['diff'], 4)
-
-        self.assertEqual(response.data['cpu']['load']['stddev']['base'], 5)
-        self.assertEqual(response.data['cpu']['load']['stddev']['target'], 9)
-        self.assertEqual(response.data['cpu']['load']['stddev']['diff'], -4)
 
     def test_compare_with_branch(self):
 
@@ -159,21 +119,24 @@ class CompareTests(APITestCase):
         branch_1 = G(models.Branch)
         branch_2 = G(models.Branch)
 
-        branch_1_result = G(models.Result, branch=branch_1)
-        branch_2_result = G(models.Result, branch=branch_2)
+        branch_1_result = G(models.Result,
+                            branch=branch_1,
+                            manifest=G(models.Manifest, manifest=MINIMAL_XML))
+
+        branch_2_result = G(models.Result,
+                            branch=branch_2,
+                            manifest=G(models.Manifest, manifest=MINIMAL_XML))
 
         G(models.ResultData,
           result=branch_1_result,
           benchmark=benchmark,
           name="load",
-
           measurement=10)
 
         G(models.ResultData,
           result=branch_2_result,
           benchmark=benchmark,
           name="load",
-
           measurement=2)
 
         response = self.client.get('/api/compare/branch/', {
@@ -196,8 +159,8 @@ class CompareTests(APITestCase):
 
         benchmark = G(models.Benchmark, name="cpu")
 
-        manifest_1 = G(models.Manifest, manifest="1")
-        manifest_2 = G(models.Manifest, manifest="2")
+        manifest_1 = G(models.Manifest, manifest=MINIMAL_XML)
+        manifest_2 = G(models.Manifest, manifest=MINIMAL_XML)
 
         manifest_1_result = G(models.Result, manifest=manifest_1)
 
@@ -214,9 +177,9 @@ class CompareTests(APITestCase):
         })
 
         self.assertEqual(response.data['cpu']['load']['avg']['base'], 10)
-        self.assertEqual(response.data['cpu']['load']['avg']['target'], None)
-        self.assertEqual(response.data['cpu']['load']['avg']['diff'], None)
+        self.assertEqual(response.data['cpu']['load']['avg']['target'], 10)
+        self.assertEqual(response.data['cpu']['load']['avg']['diff'], 0)
 
         self.assertEqual(response.data['cpu']['load']['stddev']['base'], 0)
-        self.assertEqual(response.data['cpu']['load']['stddev']['target'], None)
-        self.assertEqual(response.data['cpu']['load']['stddev']['diff'], None)
+        self.assertEqual(response.data['cpu']['load']['stddev']['target'], 0)
+        self.assertEqual(response.data['cpu']['load']['stddev']['diff'], 0)
