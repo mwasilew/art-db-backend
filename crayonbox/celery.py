d17cca51e9df2d60b324bffeccea99be0f7f24f7
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-11 18:15:30 +0100

    local settings renamed to private.py

--- crayonbox/celery.py
+++ crayonbox/celery.py
@@ -23,7 +23,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crayonbox.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crayonbox.settings.private')
 
 app = Celery('crayonbox')
 
