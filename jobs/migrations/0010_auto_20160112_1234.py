31de3d1365da2f193f91519069a9163fa20c24f7
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-12 16:21:10 +0100

    fix for migration 0010 jobs

--- jobs/migrations/0010_auto_20160112_1234.py
+++ jobs/migrations/0010_auto_20160112_1234.py
@@ -4,6 +4,27 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+
+def migrate(apps, schema_editor):
+    for item in apps.get_model("jobs", "BuildJob").objects.all():
+        if not item.gerrit_change_id:
+            item.gerrit_change_id = ""
+
+        if not item.gerrit_change_number:
+            item.gerrit_change_number = ""
+
+        if not item.gerrit_patchset_number:
+            item.gerrit_patchset_number = ""
+
+        item.save()
+
+    for item in apps.get_model("jobs", "TestJob").objects.all():
+        if not item.status:
+            item.status = ""
+
+        item.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,6 +32,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(migrate),
         migrations.AlterField(
             model_name='buildjob',
             name='gerrit_change_id',
