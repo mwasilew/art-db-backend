4a40807c9010fd4e65efecc68b12a1bce040b8c7
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-15 12:23:13 +0100

    LAVA_CREDENTIALS -> CREDENTIALS

--- ansible/templates/art_reports_local_settings.py
+++ ansible/templates/art_reports_local_settings.py
@@ -16,7 +16,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-LAVA_CREDENTIALS = {
+CREDENTIALS = {
     'validation.linaro.org': ('{{lava_user}}', '{{lava_key}}')
 }
 
