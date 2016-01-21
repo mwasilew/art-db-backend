9face87994edd47a95c25a7ef2ea1907a5d8fb8c
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-21 12:56:42 +0100

    migrations conflicts with 'testerclass-discovery' resolved

---
+++ benchmarks/migrations/0019_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('benchmarks', '0014_auto_20160120_2205'),
+        ('benchmarks', '0018_auto_20160121_1113'),
+    ]
+
+    operations = [
+    ]
