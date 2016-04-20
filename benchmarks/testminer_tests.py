7068f08639e83a44c3b464a7ed4acf49fe6e928b
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-04-20 11:31:23 -0300

    GenericLavaTestSystem: add test for failed XMLRPC calls

--- benchmarks/testminer_tests.py
+++ benchmarks/testminer_tests.py
@@ -1,7 +1,33 @@
 from django.test import TestCase
+from mock import patch
+import re
+import requests
 
+from benchmarks.testminer import GenericLavaTestSystem
 from benchmarks.testminer import LavaServerException
 from benchmarks.testminer import LavaResponseException
+
+class MockResponse(object):
+
+    def __init__(self, status_code, content):
+        self.status_code = status_code
+        self.content = content
+
+    @classmethod
+    def promise(__class__, status_code):
+        f = lambda method, url, **whatever: __class__(500, '')
+        return f
+
+
+class GenericLavaTestSystemTest(TestCase):
+
+
+    @patch("requests.request", MockResponse.promise(500))
+    def test_lava_server_request_handling(self):
+        tester = GenericLavaTestSystem('http://example.com/')
+        with self.assertRaises(LavaServerException):
+            tester.get_test_job_status(9999)
+
 
 class LavaServerExceptionTest(TestCase):
 
