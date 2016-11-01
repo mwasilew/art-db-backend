mwasilew/art-db-backend

commit 435857b8c4c4306176b8a27393be4a80f0f40531

stats: make chart lines smoother

frontend/static/main.js
@@ -294,6 +294,7 @@ app.controller('Stats', ['$scope', '$http', '$routeParams', '$timeout', '$q', '$
                     // data itself
                     series.push({
                         name: name + ' (' + env + ')',
+                        type: 'spline',
                         color: Highcharts.getOptions().colors[i],
                         zIndex: 1,
                         data: _.map(data, function(point) {
@@ -315,7 +316,7 @@ app.controller('Stats', ['$scope', '$http', '$routeParams', '$timeout', '$q', '$
                     // range of values, based on the standard deviation
                     series.push({
                         name: name + ' stdev (' + env + ')',
-                        type: 'arearange',
+                        type: 'areasplinerange',
                         color: Highcharts.getOptions().colors[i],
                         lineWidth: 0,
                         linkedTo: ':previous',

commit 8685c3404a32facffbd28ab66443ba206b07b548

avoid "build #undefined" tooltips in charts
this fixes a bug introduced by 435857b8c4c4306176b8a27393be4a80f0f40531

frontend/static/main.js
@@ -561,7 +561,7 @@ app.controller('Stats', ['$scope', '$http', '$routeParams', '$timeout', '$q', '$
                     tooltip: {
                         useHTML: true,
                         pointFormatter: function() {
-                            if (this.series.type == 'arearange') {
+                            if (this.series.type == 'areasplinerange') {
                                 return '';
                             }
                             var y = this.y.toFixed(2);