f5d5ca3706409880d2782b3a3eb71aced296e551
Antonio Terceiro <terceiro@softwarelivre.org>
2017-11-27 16:47:23 -0200

    annotations: remove if left empty

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -272,6 +272,9 @@ app.controller('BuildDetail', ['$scope', '$http', '$routeParams', '$q', '$routeP
     $scope.saving = false
     $scope.save_annotation = function() {
         $scope.saving = true
+        if ($scope.build.annotation && $scope.build.annotation.trim() == '') {
+            $scope.build.annotation = null
+        }
         $http.post(
             '/api/saveannotation/' + $scope.build.id + '/',
             { annotation: $scope.build.annotation }
